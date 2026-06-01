6c4a09285db053ab90daf0312aed0187495ba196
Dmitriy <dima345233333@yandex.ru>
2026-06-01 23:02:22 +0700

    fix and format fix_me.py

--- fix_me.py
+++ fix_me.py
@@ -1,8 +1,9 @@
-def calculate_average(nums):
-    total = sum(nums)
+def calculate_average(numbers):
+    total = sum(numbers)
     count = len(numbers)
-    average = total / count
+    return total / count
+
 
 nums = [10, 15, 20]
-result = calculat_average(nums)
-Print("The average is:", result)
+result = calculate_average(nums)
+print("The average is:", result)
